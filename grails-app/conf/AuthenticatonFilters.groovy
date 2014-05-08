a1bf954345baa4d4c8fd20fa2c4cf9b7155110c6
Utsav Popli <utsav_popli@hotmail.com>
2014-05-07 23:57:38 -0700

    made correction with the authentication

--- grails-app/conf/AuthenticatonFilters.groovy
+++ grails-app/conf/AuthenticatonFilters.groovy
@@ -5,7 +5,7 @@ class AuthenticatonFilters {
 		
 		login(action:"*", controller: "*"){
 			before = {
-				if(!session.user && actionName=='addToCart' ||actionName=='MyCart' || actionName=='MyAccount' || actionName=='removeFromCart' )
+				if(!session.user && (actionName=='addToCart' ||actionName=='MyCart' || actionName=='MyAccount' || actionName=='removeFromCart') )
 				{
 					redirect(action:"login")
 					return false
@@ -19,7 +19,7 @@ class AuthenticatonFilters {
 		retail(action: '*', controller:'*')
 		{
 			before ={
-				if((session.user=='retail' || !session.user) && (controllerName == 'user' || controllerName == 'address' || controllerName == 'productsDisplay' || controllerName =='cart'))
+				if((session.user=='retail') && (controllerName == 'user' || controllerName == 'address' || controllerName == 'productsDisplay' || controllerName =='cart'))
 			
 				{
 					render "error 403 You are Forbidden to Enter"
