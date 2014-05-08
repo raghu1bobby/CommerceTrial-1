9111d2efc32e8a44168a3e9178d241e09839ae82
Utsav Popli <utsav_popli@hotmail.com>
2014-05-07 23:46:11 -0700

    admin and retailer authentication

---
+++ grails-app/conf/AuthenticatonFilters.groovy
@@ -0,0 +1,39 @@
+
+class AuthenticatonFilters {
+
+	def filters = {
+		
+		login(action:"*", controller: "*"){
+			before = {
+				if(!session.user && actionName=='addToCart' ||actionName=='MyCart' || actionName=='MyAccount' || actionName=='removeFromCart' )
+				{
+					redirect(action:"login")
+					return false
+				}
+				
+			}
+			
+			
+		}
+		
+		retail(action: '*', controller:'*')
+		{
+			before ={
+				if((session.user=='retail' || !session.user) && (controllerName == 'user' || controllerName == 'address' || controllerName == 'productsDisplay' || controllerName =='cart'))
+			
+				{
+					render "error 403 You are Forbidden to Enter"
+					return false
+				}
+			}
+			
+
+		}
+
+		
+				
+		
+		
+	}
+	
+}
--- grails-app/controllers/commercetrial/UserActionController.groovy
+++ grails-app/controllers/commercetrial/UserActionController.groovy
@@ -15,9 +15,12 @@ def index = {
 	def login = {
 		println params.username
 		
-		if(params.username == "retail" && params.password =="retail")
+		
+		
+		
+		if((params.username == "retail" && params.password =="retail") || (params.username=='admin' && params.password =='admin'))
 		{
-			session.user = "retail"
+			session.user = params.username
 			render(view:"/index")
 		}else {
 		
@@ -84,5 +87,42 @@ println params.username
 		}
 		
 	}
+	
+	
+	def addToCart = {
+		def idx = params.id;
+		println params
+		//println session?.user.username
+		
+		def u = User.findByUsername(session?.user.username)
+		println u.username
+		if (u) {
+			println "TEST"
+			
+			//flash.total = Shared
+			
+			
+			def priceFind = Shared_db.getAll(u.Mycart)
+			println priceFind.Price
+			flash.message= priceFind.Price
+			
+			u.addToMycart(idx).save()
+			println u['counter']
+			//u['counter']= u['counter']+1
+			u.counter++;
+			u.save(flush:true);
+			session.user = u
+			redirect(controller: "laptop", action: "showAll")
+			
+			//redirect(controller:'main')
+		} else {
+		println "bobby3"
+			flash.message = "Please Login to Add Items to the Cart"
+			render (view : "login")
+			//redirect(controller:'user')
+		}
+		
+	}
+	
 	
 	}
